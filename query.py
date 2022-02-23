ec3f8c87909c5365314acedafe208992983f1f4e
wchr-aun <wchr.aun@gmail.com>
2022-02-23 03:56:50 +0000

    feat(query): to locally save the reviews after the query

--- query.py
+++ query.py
@@ -1,6 +1,35 @@
+import pickle
+import os
+
+
+FILENAME = f'./store/reviews.pkl'
+
+
 def query_test_reviews(client):
     reviews = []
-    for review in client.find({}):
-        reviews.append(
-            {'review': review['review_text'], 'id': review['review_id'], 'book_id': review['book']['book_id']})
+    if os.path.isfile(FILENAME):
+        reviews = _load_reviews()
+    else:
+        for review in client.find({}):
+            reviews.append(
+                {
+                    'review': review['review_text'],
+                    'id': review['review_id'],
+                    'book_id': review['book']['book_id'],
+                    'review_count': review['book']['text_reviews_count']
+                })
+        _save_reviews(reviews)
+    return reviews
+
+
+def _save_reviews(reviews):
+    with open(FILENAME, 'wb') as output:
+        pickle.dump(reviews, output, pickle.HIGHEST_PROTOCOL)
+    print('Saved.')
+
+
+def _load_reviews():
+    with open(FILENAME, 'rb') as input:
+        reviews = pickle.load(input)
+    print('Loaded.')
     return reviews
